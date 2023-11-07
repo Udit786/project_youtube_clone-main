bf83564591621c00ee0ac5c4b2ade8367618ca94
pratikshatrixie <pratikshatrixie@gmail.com>
2023-11-07 22:39:36 +0530

    profile

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import ProfileDropdown from './ProfileDropdown'; // Import the ProfileDropdown component
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -12,33 +13,49 @@ const SearchBar = () => {
 
     if (searchTerm) {
       navigate(`/search/${searchTerm}`);
-
       setSearchTerm('');
     }
   };
 
   return (
-    <Paper
-      component='form'
-      onSubmit={onhandleSubmit}
-      sx={{
-        borderRadius: 20,
-        border: '1px solid #e3e3e3',
-        pl: 2,
-        boxShadow: 'none',
-        mr: { sm: 5 },
-      }}
-    >
-      <input
-        className='search-bar'
-        placeholder='Search...'
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
-        <SearchIcon />
-      </IconButton>
-    </Paper>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <Paper
+        component='form'
+        onSubmit={onhandleSubmit}
+        sx={{
+          borderRadius: 20,
+          border: '1px solid #e3e3e3',
+          pl: 2,
+          boxShadow: 'none',
+          mr: { sm: 5 },
+        }}
+      >
+        <input
+          className='search-bar'
+          placeholder='Search...'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <IconButton type='submit' aria-label='search'>
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      <div
+        style={{
+          width: '40px',
+          height: '40px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '50%',
+          border: '2px solid #e74c3c', // Red outer border
+          cursor: 'pointer',
+        }}
+      >
+        {/* You can place an icon or text inside this div for the profile icon */}
+      </div>
+      <ProfileDropdown />
+    </div>
   );
 };
 
